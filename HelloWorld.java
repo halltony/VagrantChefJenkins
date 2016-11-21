3b1b4f1c1fe0bdf1f32aaebd524e330488f739f1
halltony <a.j.hall@blueyonder.co.uk>
2016-11-21 19:54:38 +0000

    Change message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Tony");
+        System.out.println("Hello, third time lucky Tony");
     }
 
 }
